14732021af8cbc6d06cf89298e50eed80d639f25
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2022-01-16 22:29:33 +0100

    added user model class

---
+++ DataAccess/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models;
+
+internal class UserModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
