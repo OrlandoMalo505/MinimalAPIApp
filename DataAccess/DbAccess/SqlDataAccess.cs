d1b9765667d9084ac61a582490be239a6840c537
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2022-01-17 22:38:14 +0100

    added SqlDataAccess class

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess;
+
+public  class SqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config )
+    {
+        _config = config;
+    }
+}
