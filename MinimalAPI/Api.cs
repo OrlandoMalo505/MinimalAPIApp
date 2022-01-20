1216648b028acb1c2b6ecd4d61ad10e3b9fe054f
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2022-01-20 21:59:00 +0100

    added get endpoints

--- MinimalAPI/Api.cs
+++ MinimalAPI/Api.cs
@@ -5,6 +5,38 @@ public static class Api
 
     public static void ConfigureApi(this WebApplication app)
     {
+        app.MapGet("/Users", GetUsers);
+        app.MapGet("/Users/{id}", GetUser);
+    }
 
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetUsers());
+
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
+
+    }
+
+    private static async Task<IResult> GetUser(int id, IUserData data)
+    {
+        try
+        {
+            var result=await data.GetUser(id);
+            if (result == null) return Results.NotFound();
+            return Results.Ok(result);
+
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
     }
 }
